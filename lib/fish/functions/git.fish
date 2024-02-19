eeeef9a3f6af0e57928d98002c0478f394aaa2df
Michael Krebs <michikrbs@gmail.com>
2024-02-19 21:30:07 +0100

    (fish)[functions]: add git safe

--- lib/fish/functions/git.fish
+++ lib/fish/functions/git.fish
@@ -2,6 +2,8 @@ function git
     if test -n "$argv"
         if test $argv[1] = gud
             command echo "git out of here!"
+        else if test $argv[1] = safe
+            command git config --global --add safe.directory $(realpath .)
         else
             command git $argv
         end
