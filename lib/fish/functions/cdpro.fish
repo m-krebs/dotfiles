7b21dcb5ba36f193fd43725f97069918cca30466
Michael Krebs <michael.krebs@xpublisher.com>
2024-04-30 18:05:27 +0200

    (fish)[function]: remove cdpro, config

--- lib/fish/functions/cdpro.fish
+++
@@ -1,4 +0,0 @@
-function cdpro --wraps='cd /home/mkrebs/Documents/ProjectFiles' --description 'alias cdpro=cd /home/mkrebs/Documents/ProjectFiles'
-  cd /home/mkrebs/Documents/ProjectFiles $argv
-        
-end
--- lib/fish/functions/config.fish
+++
@@ -1,4 +0,0 @@
-function config --wraps='/usr/bin/git --git-dir=/home/mkrebs/.cfg/ --work-tree=/home/mkrebs' --description 'alias config=git --git-dir=/home/mkrebs/.cfg/ --work-tree=/home/mkrebs'
-    /usr/bin/git --git-dir=/home/mkrebs/.cfg/ --work-tree=/home/mkrebs $argv
-
-end
