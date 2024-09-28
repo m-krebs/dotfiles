25bec64adf33467ce2ca63de15f0d5f9f0e97b6d
Michael Krebs <michikrbs@gmail.com>
2024-09-28 18:24:57 +0200

    feat: add polybar

---
+++ private_dot_config/polybar/executable_launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload default &
+	done
+else
+	polybar --reload toph &
+fi
