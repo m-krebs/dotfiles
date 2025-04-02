31e324a87a892538135696c30f3ebdfc22fba83b
Michael Krebs <michael.krebs@xpublisher.com>
2025-04-02 11:06:35 +0200

    fish(abbr): add git commit/diff, replace unlighthouse function with ulh abbr

--- lib/fish/conf.d/abbr.fish
+++ lib/fish/conf.d/abbr.fish
@@ -1,2 +1,5 @@
 abbr -a --position anywhere --command git -- co checkout
 abbr -a --position anywhere --command git -- st status
+abbr -a --position anywhere --command git -- d diff
+abbr -a --position anywhere --command git -- c commit
+abbr -a -- ulh 'pnpm dlx unlighthouse --output-path ~/unlighthouse --site'
