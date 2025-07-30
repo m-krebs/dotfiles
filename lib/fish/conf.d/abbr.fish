6b2a45a61c43a9686b8fe71d117997e094b04e95
Michael Krebs <michikrbs@gmail.com>
2025-07-30 19:21:46 +0200

    chore(fish): add abbr

--- lib/fish/conf.d/abbr.fish
+++ lib/fish/conf.d/abbr.fish
@@ -4,3 +4,4 @@ abbr -a --position anywhere --command git -- d diff
 abbr -a --position anywhere --command git -- c commit
 abbr -a -- ulh 'pnpm dlx unlighthouse --output-path ~/unlighthouse --site'
 abbr -a --position anywhere -- nv nvim
+abbr -a -- zj 'zellij attach default'
