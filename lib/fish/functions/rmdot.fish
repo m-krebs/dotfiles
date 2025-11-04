7e369f8d37345c80649e292b996f2d8ce7d52db3
Michael Krebs <michikrbs@gmail.com>
2025-11-04 11:52:34 +0100

    chore: refactor rmdot

--- lib/fish/functions/rmdot.fish
+++ lib/fish/functions/rmdot.fish
@@ -1,5 +1,12 @@
 function rmdot
+        echo -e "###############################################\n--> $(pwd) <--\n###############################################"
+        read -P "Are you sure you want to delete this directory? (y/N): " answer
+        if test "$answer" != "y"
+                echo "Cancelled by user."
+                return
+        end
         set dir_to_delete $(pwd)
         cd ..
         rm -rf $dir_to_delete
+        echo "directory is gone."
 end
