e848e35c82bd6e327d070e8a6f4767546ebf82ea
Michael Krebs <michikrbs@gmail.com>
2026-06-08 16:08:46 +0200

    fix(fish): helm completion

--- lib/fish/completions/helm.fish
+++ lib/fish/completions/helm.fish
@@ -1,27 +1,27 @@
-# fish completion for diff                                 -*- shell-script -*-
+# fish completion for helm                                 -*- shell-script -*-
 
-function __diff_debug
+function __helm_debug
     set -l file "$BASH_COMP_DEBUG_FILE"
     if test -n "$file"
         echo "$argv" >> $file
     end
 end
 
-function __diff_perform_completion
-    __diff_debug "Starting __diff_perform_completion"
+function __helm_perform_completion
+    __helm_debug "Starting __helm_perform_completion"
 
     # Extract all args except the last one
     set -l args (commandline -opc)
     # Extract the last arg and escape it in case it is a space
     set -l lastArg (string escape -- (commandline -ct))
 
-    __diff_debug "args: $args"
-    __diff_debug "last arg: $lastArg"
+    __helm_debug "args: $args"
+    __helm_debug "last arg: $lastArg"
 
     # Disable ActiveHelp which is not supported for fish shell
-    set -l requestComp "DIFF_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
+    set -l requestComp "HELM_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
 
-    __diff_debug "Calling $requestComp"
+    __helm_debug "Calling $requestComp"
     set -l results (eval $requestComp 2> /dev/null)
 
     # Some programs may output extra empty lines after the directive.
@@ -44,9 +44,9 @@ function __diff_perform_completion
     # completions must be prefixed with the flag
     set -l flagPrefix (string match -r -- '-.*=' "$lastArg")
 
-    __diff_debug "Comps: $comps"
-    __diff_debug "DirectiveLine: $directiveLine"
-    __diff_debug "flagPrefix: $flagPrefix"
+    __helm_debug "Comps: $comps"
+    __helm_debug "DirectiveLine: $directiveLine"
+    __helm_debug "flagPrefix: $flagPrefix"
 
     for comp in $comps
         printf "%s%s\n" "$flagPrefix" "$comp"
@@ -55,84 +55,84 @@ function __diff_perform_completion
     printf "%s\n" "$directiveLine"
 end
 
-# this function limits calls to __diff_perform_completion, by caching the result behind $__diff_perform_completion_once_result
-function __diff_perform_completion_once
-    __diff_debug "Starting __diff_perform_completion_once"
+# this function limits calls to __helm_perform_completion, by caching the result behind $__helm_perform_completion_once_result
+function __helm_perform_completion_once
+    __helm_debug "Starting __helm_perform_completion_once"
 
-    if test -n "$__diff_perform_completion_once_result"
-        __diff_debug "Seems like a valid result already exists, skipping __diff_perform_completion"
+    if test -n "$__helm_perform_completion_once_result"
+        __helm_debug "Seems like a valid result already exists, skipping __helm_perform_completion"
         return 0
     end
 
-    set --global __diff_perform_completion_once_result (__diff_perform_completion)
-    if test -z "$__diff_perform_completion_once_result"
-        __diff_debug "No completions, probably due to a failure"
+    set --global __helm_perform_completion_once_result (__helm_perform_completion)
+    if test -z "$__helm_perform_completion_once_result"
+        __helm_debug "No completions, probably due to a failure"
         return 1
     end
 
-    __diff_debug "Performed completions and set __diff_perform_completion_once_result"
+    __helm_debug "Performed completions and set __helm_perform_completion_once_result"
     return 0
 end
 
-# this function is used to clear the $__diff_perform_completion_once_result variable after completions are run
-function __diff_clear_perform_completion_once_result
-    __diff_debug ""
-    __diff_debug "========= clearing previously set __diff_perform_completion_once_result variable =========="
-    set --erase __diff_perform_completion_once_result
-    __diff_debug "Successfully erased the variable __diff_perform_completion_once_result"
+# this function is used to clear the $__helm_perform_completion_once_result variable after completions are run
+function __helm_clear_perform_completion_once_result
+    __helm_debug ""
+    __helm_debug "========= clearing previously set __helm_perform_completion_once_result variable =========="
+    set --erase __helm_perform_completion_once_result
+    __helm_debug "Successfully erased the variable __helm_perform_completion_once_result"
 end
 
-function __diff_requires_order_preservation
-    __diff_debug ""
-    __diff_debug "========= checking if order preservation is required =========="
+function __helm_requires_order_preservation
+    __helm_debug ""
+    __helm_debug "========= checking if order preservation is required =========="
 
-    __diff_perform_completion_once
-    if test -z "$__diff_perform_completion_once_result"
-        __diff_debug "Error determining if order preservation is required"
+    __helm_perform_completion_once
+    if test -z "$__helm_perform_completion_once_result"
+        __helm_debug "Error determining if order preservation is required"
         return 1
     end
 
-    set -l directive (string sub --start 2 $__diff_perform_completion_once_result[-1])
-    __diff_debug "Directive is: $directive"
+    set -l directive (string sub --start 2 $__helm_perform_completion_once_result[-1])
+    __helm_debug "Directive is: $directive"
 
     set -l shellCompDirectiveKeepOrder 32
     set -l keeporder (math (math --scale 0 $directive / $shellCompDirectiveKeepOrder) % 2)
-    __diff_debug "Keeporder is: $keeporder"
+    __helm_debug "Keeporder is: $keeporder"
 
     if test $keeporder -ne 0
-        __diff_debug "This does require order preservation"
+        __helm_debug "This does require order preservation"
         return 0
     end
 
-    __diff_debug "This doesn't require order preservation"
+    __helm_debug "This doesn't require order preservation"
     return 1
 end
 
 
 # This function does two things:
-# - Obtain the completions and store them in the global __diff_comp_results
+# - Obtain the completions and store them in the global __helm_comp_results
 # - Return false if file completion should be performed
-function __diff_prepare_completions
-    __diff_debug ""
-    __diff_debug "========= starting completion logic =========="
+function __helm_prepare_completions
+    __helm_debug ""
+    __helm_debug "========= starting completion logic =========="
 
     # Start fresh
-    set --erase __diff_comp_results
+    set --erase __helm_comp_results
 
-    __diff_perform_completion_once
-    __diff_debug "Completion results: $__diff_perform_completion_once_result"
+    __helm_perform_completion_once
+    __helm_debug "Completion results: $__helm_perform_completion_once_result"
 
-    if test -z "$__diff_perform_completion_once_result"
-        __diff_debug "No completion, probably due to a failure"
+    if test -z "$__helm_perform_completion_once_result"
+        __helm_debug "No completion, probably due to a failure"
         # Might as well do file completion, in case it helps
         return 1
     end
 
-    set -l directive (string sub --start 2 $__diff_perform_completion_once_result[-1])
-    set --global __diff_comp_results $__diff_perform_completion_once_result[1..-2]
+    set -l directive (string sub --start 2 $__helm_perform_completion_once_result[-1])
+    set --global __helm_comp_results $__helm_perform_completion_once_result[1..-2]
 
-    __diff_debug "Completions are: $__diff_comp_results"
-    __diff_debug "Directive is: $directive"
+    __helm_debug "Completions are: $__helm_comp_results"
+    __helm_debug "Directive is: $directive"
 
     set -l shellCompDirectiveError 1
     set -l shellCompDirectiveNoSpace 2
@@ -146,7 +146,7 @@ function __diff_prepare_completions
 
     set -l compErr (math (math --scale 0 $directive / $shellCompDirectiveError) % 2)
     if test $compErr -eq 1
-        __diff_debug "Received error directive: aborting."
+        __helm_debug "Received error directive: aborting."
         # Might as well do file completion, in case it helps
         return 1
     end
@@ -154,7 +154,7 @@ function __diff_prepare_completions
     set -l filefilter (math (math --scale 0 $directive / $shellCompDirectiveFilterFileExt) % 2)
     set -l dirfilter (math (math --scale 0 $directive / $shellCompDirectiveFilterDirs) % 2)
     if test $filefilter -eq 1; or test $dirfilter -eq 1
-        __diff_debug "File extension filtering or directory filtering not supported"
+        __helm_debug "File extension filtering or directory filtering not supported"
         # Do full file completion instead
         return 1
     end
@@ -162,7 +162,7 @@ function __diff_prepare_completions
     set -l nospace (math (math --scale 0 $directive / $shellCompDirectiveNoSpace) % 2)
     set -l nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
 
-    __diff_debug "nospace: $nospace, nofiles: $nofiles"
+    __helm_debug "nospace: $nospace, nofiles: $nofiles"
 
     # If we want to prevent a space, or if file completion is NOT disabled,
     # we need to count the number of valid completions.
@@ -171,22 +171,22 @@ function __diff_prepare_completions
     # criteria than the prefix.
     if test $nospace -ne 0; or test $nofiles -eq 0
         set -l prefix (commandline -t | string escape --style=regex)
-        __diff_debug "prefix: $prefix"
+        __helm_debug "prefix: $prefix"
 
-        set -l completions (string match -r -- "^$prefix.*" $__diff_comp_results)
-        set --global __diff_comp_results $completions
-        __diff_debug "Filtered completions are: $__diff_comp_results"
+        set -l completions (string match -r -- "^$prefix.*" $__helm_comp_results)
+        set --global __helm_comp_results $completions
+        __helm_debug "Filtered completions are: $__helm_comp_results"
 
         # Important not to quote the variable for count to work
-        set -l numComps (count $__diff_comp_results)
-        __diff_debug "numComps: $numComps"
+        set -l numComps (count $__helm_comp_results)
+        __helm_debug "numComps: $numComps"
 
         if test $numComps -eq 1; and test $nospace -ne 0
             # We must first split on \t to get rid of the descriptions to be
             # able to check what the actual completion will be.
             # We don't need descriptions anyway since there is only a single
             # real completion which the shell will expand immediately.
-            set -l split (string split --max 1 \t $__diff_comp_results[1])
+            set -l split (string split --max 1 \t $__helm_comp_results[1])
 
             # Fish won't add a space if the completion ends with any
             # of the following characters: @=/:.,
@@ -194,16 +194,16 @@ function __diff_prepare_completions
             if not string match -r -q "[@=/:.,]" -- "$lastChar"
                 # In other cases, to support the "nospace" directive we trick the shell
                 # by outputting an extra, longer completion.
-                __diff_debug "Adding second completion to perform nospace directive"
-                set --global __diff_comp_results $split[1] $split[1].
-                __diff_debug "Completions are now: $__diff_comp_results"
+                __helm_debug "Adding second completion to perform nospace directive"
+                set --global __helm_comp_results $split[1] $split[1].
+                __helm_debug "Completions are now: $__helm_comp_results"
             end
         end
 
         if test $numComps -eq 0; and test $nofiles -eq 0
             # To be consistent with bash and zsh, we only trigger file
             # completion when there are no other completions
-            __diff_debug "Requesting file completion"
+            __helm_debug "Requesting file completion"
             return 1
         end
     end
@@ -215,21 +215,21 @@ end
 # so we can properly delete any completions provided by another script.
 # Only do this if the program can be found, or else fish may print some errors; besides,
 # the existing completions will only be loaded if the program can be found.
-if type -q "diff"
+if type -q "helm"
     # The space after the program name is essential to trigger completion for the program
     # and not completion of the program name itself.
     # Also, we use '> /dev/null 2>&1' since '&>' is not supported in older versions of fish.
-    complete --do-complete "diff " > /dev/null 2>&1
+    complete --do-complete "helm " > /dev/null 2>&1
 end
 
 # Remove any pre-existing completions for the program since we will be handling all of them.
-complete -c diff -e
+complete -c helm -e
 
-# this will get called after the two calls below and clear the $__diff_perform_completion_once_result global
-complete -c diff -n '__diff_clear_perform_completion_once_result'
-# The call to __diff_prepare_completions will setup __diff_comp_results
+# this will get called after the two calls below and clear the $__helm_perform_completion_once_result global
+complete -c helm -n '__helm_clear_perform_completion_once_result'
+# The call to __helm_prepare_completions will setup __helm_comp_results
 # which provides the program's completion choices.
 # If this doesn't require order preservation, we don't use the -k flag
-complete -c diff -n 'not __diff_requires_order_preservation && __diff_prepare_completions' -f -a '$__diff_comp_results'
+complete -c helm -n 'not __helm_requires_order_preservation && __helm_prepare_completions' -f -a '$__helm_comp_results'
 # otherwise we use the -k flag
-complete -k -c diff -n '__diff_requires_order_preservation && __diff_prepare_completions' -f -a '$__diff_comp_results'
+complete -k -c helm -n '__helm_requires_order_preservation && __helm_prepare_completions' -f -a '$__helm_comp_results'
